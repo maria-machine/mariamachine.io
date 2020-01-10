13fbfd2dadabc078453375cfd7e7910ccf15eab8
Roman Ponomarev <maksugr@gmail.com>
2020-01-10 19:33:49 +0300

    Move to typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,42 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Maria Machine',
-    author: 'Roman Ponomarev',
-    description: '',
-    siteUrl: 'https://mariamachine.io',
-    social: {
-      twitter: 'mariamachine_ml',
-      facebook: 'maria.machine.ml',
-      vk: 'maria_machine',
-      telegram: 'maria_machine'
-    }
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: "Maria Machine",
+        author: "Roman Ponomarev",
+        description: "",
+        siteUrl: "https://mariamachine.io",
+        social: {
+            twitter: "mariamachine_ml",
+            facebook: "maria.machine.ml",
+            vk: "maria_machine",
+            telegram: "maria_machine",
+        },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
