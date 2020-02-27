20c8041b98a63ca177790806718ec3df40a3a720
Roman Ponomarev <maksugr@gmail.com>
2020-02-27 15:26:43 +0300

    Fix intermidiate changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,43 @@
 module.exports = {
     siteMetadata: {
-        title: "Maria Machine",
-        author: "Roman Ponomarev",
-        description: "",
-        siteUrl: "https://mariamachine.io",
+        title: 'Maria Machine',
+        author: 'Roman Ponomarev',
+        description: '',
+        siteUrl: 'https://mariamachine.io',
         social: {
-            twitter: "mariamachine_ml",
-            facebook: "maria.machine.ml",
-            vk: "maria_machine",
-            telegram: "maria_machine",
-        },
+            twitter: 'mariamachine_ml',
+            facebook: 'maria.machine.ml',
+            vk: 'maria_machine',
+            telegram: 'maria_machine'
+        }
     },
     plugins: [
         `gatsby-plugin-typescript`,
+        `gatsby-plugin-styled-components`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images`,
-            },
+                path: `${__dirname}/src/images`
+            }
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Maria Machine`,
+                short_name: `Maria Machine`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#ffffff`,
+                theme_color: `#f6e088`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
+                icon: `src/images/maria-machine.png`
+            }
+        }
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
-    ],
-}
+    ]
+};
